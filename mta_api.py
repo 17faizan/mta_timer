01f86acc9ba1c9a6cfd9b4bd9c6ca8324875fe72
Faizan Shamsi <69012556+17faizan@users.noreply.github.com>
2025-12-28 22:07:07 -0500

    Update MTA API logic

--- mta_api.py
+++ mta_api.py
@@ -15,15 +15,17 @@ def fetch_realtime_data():
         print(f"Error: {response.status_code}")
         return None
 
+
+
+print(feed.entity[0])
+
 def display_train_updates(feed):
     for entity in feed.entity:
         if entity.HasField("trip_update"):
             trip_update = entity.trip_update
             for stop_time_update in trip_update.stop_time_update:
-
                 if stop_time_update.stop_id.startswith("602"):
                     print(f"Trip ID: {trip_update.trip.trip_id}")
-
                     print(f"  Stop ID: {stop_time_update.stop_id}")
                     print(f"  Arrival Time: {stop_time_update.arrival.time}")
                     print(f"  Departure Time: {stop_time_update.departure.time}")
